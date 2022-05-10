e97921984bf7a0c3a1aa3e587012a88bb567fd17
Ata Kuyumcu <ata@mm.st>
2022-05-10 15:35:01 +0300

    added integration tests for url query parameter validations

--- tests/integration/suggestions.ts
+++ tests/integration/suggestions.ts
@@ -11,15 +11,28 @@ async function fetchSuggestions(query: Partial<SuggestionArguments>): Promise<Su
     query: Object.fromEntries(Object.entries({ sort: "name", ...query }).map(([key, value]) => [key, value.toString()]))
   })
 
+  if (response.statusCode !== 200) {
+    throw new Error(response.body)
+  }
+
   return response.json<SuggestionsResponse>().suggestions as Suggestion[]
 }
 
 describe("/suggestions", () => {
+  it("should not accept empty or undefined query argument", async () => {
+    expect(() => fetchSuggestions({ query: "" })).rejects
+    expect(() => fetchSuggestions({})).rejects
+  })
+
+  it("should not accept partial positional arguments", async () => {
+    expect(fetchSuggestions({ query: "London", longitude: 45.23 })).rejects
+    expect(fetchSuggestions({ query: "London", latitude: -58.21 })).rejects
+    expect(fetchSuggestions({ query: "London", radius: 3 })).rejects
+  })
+
   it("should return correct suggestions for non-positional query", async () => {
     const suggestions = await fetchSuggestions({ query: "London" })
-    expect(suggestions).toIncludeAllMembers(cities
-      .filter((city) => /London/i.test(city.name))
-      .map((city) => cityToSuggestion(city)))
+    expect(suggestions).toIncludeAllMembers(cities.filter((city) => /London/i.test(city.name)).map((city) => cityToSuggestion(city)))
   })
 
   it("should return the correct suggestion for positional query with a small radius", async () => {
